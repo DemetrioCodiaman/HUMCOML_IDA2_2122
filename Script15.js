3a759ffc57961592d6007c6607da2a1e7a573bab
DemetrioCodiaman_HUMCOML_IDA2_2122 <97889847+DemetrioCodiaman@users.noreply.github.com>
2022-05-13 21:43:54 +0800

    Add files via upload

---
+++ Script15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Demetrio Codiaman";
+    var age = document.getElementById("age").value = "19";
+    var phoneNumber = document.getElementById("phoneNumber").value = "09495976558";
+    var eMail = document.getElementById("eMail").value = "20216950@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "Longlong Puguis La Trinidad Benguet";
+    var program = document.getElementById("program").value = "Information Technology";
+    var idNum = document.getElementById("idNum").value = "20216950";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IDA2";
+}
